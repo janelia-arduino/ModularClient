956fe479f23b24e01b5491c07977d113b66aaf96
Peter Polidoro <peterpolidoro@gmail.com>
2015-12-10 15:02:11 -0500

    Updated LedControllerClient.ino.

--- examples/LedControllerClient/LedControllerClient.ino
+++ examples/LedControllerClient/LedControllerClient.ino
@@ -1,30 +1,60 @@
-// LedController
+// LedControllerClient
 #include "Flash.h"
 #include <EEPROM.h>
 #include "Streaming.h"
 #include "ArduinoJson.h"
-#include "JsonSanitizer.h"
 #include "JsonStream.h"
 #include "Array.h"
-#include "MemoryFree.h"
 #include "ConstantVariable.h"
-#include "SavedVariable.h"
-#include "GenericSerial.h"
-#include "ModularDevice.h"
-#include "NonBlockBlink.h"
-#include "Constants.h"
-#include "Callbacks.h"
-#include "Controller.h"
+#include "ModularClient.h"
 
 
-// See README.md for more information
+const int BAUDRATE = 9600;
+ModularClient modular_client(Serial2);
+const unsigned char serial2_rx_pin = 17;
 
 void setup()
 {
-  controller.setup();
+  Serial.begin(BAUDRATE);
+  Serial2.begin(BAUDRATE);
+  pinMode(serial2_rx_pin,INPUT);
+  digitalWrite(serial2_rx_pin,HIGH);
+}
+
+void checkCall(const char method[])
+{
+  if (modular_client.callWasSuccessful())
+  {
+    Serial << method << " success!" << "\n";
+  }
+  else
+  {
+    Serial << method << " error!" << "\n";
+  }
+  Serial << "\n";
 }
 
 void loop()
 {
-  controller.update();
+  long led_pin = modular_client.callServerMethod("getLedPin");
+  checkCall("getLedPin");
+  if (modular_client.callWasSuccessful())
+  {
+    Serial << "led_pin = " << led_pin << "\n";
+    Serial << "\n";
+  }
+  delay(1000);
+
+  modular_client.callServerMethod("setLedOn");
+  checkCall("setLedOn");
+  delay(1000);
+
+  modular_client.callServerMethod("setLedOff");
+  checkCall("setLedOff");
+  delay(1000);
+
+  modular_client.callServerMethod("blinkLed",0.5,0.5,10);
+  checkCall("blinkLed");
+  delay(10000);
+
 }
