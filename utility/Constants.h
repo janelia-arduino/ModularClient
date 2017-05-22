ace8e1eb047627b0208f4b42de5f2239905cdf32
Peter Polidoro <peterpolidoro@gmail.com>
2017-05-22 10:05:27 -0400

    Reduce ADDRESS_ID_COUNT_MAX on mega to reduce memory footprint.

--- utility/Constants.h
+++ utility/Constants.h
@@ -14,7 +14,11 @@ namespace modular_client
 {
 namespace constants
 {
+#if !defined(__AVR_ATmega2560__)
 enum{ADDRESS_ID_COUNT_MAX=16};
+#else
+enum{ADDRESS_ID_COUNT_MAX=1};
+#endif
 
 extern ConstantString result_key_string;
 
